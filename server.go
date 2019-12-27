1bd43b9df75e0847755e7172674aca52db5da165
xsymphony <xiongshengyao@hotmail.com>
2019-12-27 11:42:27 +0800

    [v0.0.1]添加路由分发的原型

--- server.go
+++ server.go
@@ -2,8 +2,21 @@ package main
 
 import "github.com/valyala/fasthttp"
 
-func main() {
-    fasthttp.ListenAndServe(":8080", func(ctx *fasthttp.RequestCtx) {
+
+// dispatch 是路由分发的原型
+// 根据请求路由执行不同的处理逻辑
+func dispatch(ctx *fasthttp.RequestCtx) {
+    switch string(ctx.Path()) {
+    case "/api/v1/hello":
         ctx.WriteString("hello world")
-    })
+    case "/api/v2/hello":
+        ctx.WriteString("你好")
+    default:
+        ctx.SetStatusCode(404)
+        ctx.WriteString("404 NOT FOUND")
+    }
+}
+
+func main() {
+    fasthttp.ListenAndServe(":8080", dispatch)
 }
