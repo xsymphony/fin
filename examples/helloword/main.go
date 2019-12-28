aebca62d92c1cf355ecbe02350bc3bcaf5849908
xsymphony <xiongshengyao@hotmail.com>
2019-12-28 17:36:51 +0800

    [v0.0.2]增加反垃圾的示例

--- examples/helloword/main.go
+++ examples/helloword/main.go
@@ -1,19 +1,19 @@
 package main
 
 import (
-    "github.com/valyala/fasthttp"
-    "github.com/xsymphony/fin"
+	"github.com/valyala/fasthttp"
+	"github.com/xsymphony/fin"
 )
 
 func main() {
-    r := fin.NewRouter("/api")
-    {
-        r.AddRouter("/v1/hello", func(ctx *fasthttp.RequestCtx) {
-            ctx.WriteString("hello world")
-        })
-        r.AddRouter("/v2/hello",func(ctx *fasthttp.RequestCtx) {
-            ctx.WriteString("你好")
-        })
-    }
-    fasthttp.ListenAndServe(":8080", r.HandleRequest)
+	r := fin.NewRouter("/api")
+	{
+		r.AddRouter("/v1/hello", func(ctx *fasthttp.RequestCtx) {
+			ctx.WriteString("hello world")
+		})
+		r.AddRouter("/v2/hello", func(ctx *fasthttp.RequestCtx) {
+			ctx.WriteString("你好")
+		})
+	}
+	fasthttp.ListenAndServe(":8080", r.HandleRequest)
 }
