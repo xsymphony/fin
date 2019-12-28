f58e2445ac8f190642cf5d807850aa9e1242d7e3
xsymphony <xiongshengyao@hotmail.com>
2019-12-28 13:31:28 +0800

    [v0.0.2b]修改adapter的类型 struct -> func

--- adapter.go
+++ adapter.go
@@ -2,18 +2,10 @@ package fin
 
 import "github.com/valyala/fasthttp"
 
-// HandlerAdapter是通用的handler函数适配器
+// HandlerFunc是通用的handler函数适配器
 // 使外层调用不再用申明结构体
-type HandlerAdapter struct {
-    f func(ctx *fasthttp.RequestCtx)
-}
+type HandlerFunc func(ctx *fasthttp.RequestCtx)
 
-func (adapter *HandlerAdapter) Serve(ctx *fasthttp.RequestCtx) {
-    adapter.f(ctx)
-}
-
-func NewAdapter(f func(ctx *fasthttp.RequestCtx)) *HandlerAdapter {
-    return &HandlerAdapter{
-        f: f,
-    }
+func (f HandlerFunc) Serve(ctx *fasthttp.RequestCtx) {
+	f(ctx)
 }
--- examples/helloword/main.go
+++ examples/helloword/main.go
@@ -1,23 +1,23 @@
 package main
 
 import (
-    "github.com/valyala/fasthttp"
-    "github.com/xsymphony/fin"
+	"github.com/valyala/fasthttp"
+	"github.com/xsymphony/fin"
 )
 
 func main() {
-    r := fin.Default("/api")
-    r.Use(fin.AllowMethodMiddleware("GET"))
-    {
-        r.ANY("/v1/hello", fin.NewAdapter(func(ctx *fasthttp.RequestCtx) {
-            ctx.WriteString("hello world")
-        }))
-        r.ANY("/v2/hello", fin.NewAdapter(func(ctx *fasthttp.RequestCtx) {
-            ctx.WriteString("你好")
-        }))
-        r.GET("/panic", fin.NewAdapter(func(ctx *fasthttp.RequestCtx) {
-            panic("just for panic")
-        }))
-    }
-    fasthttp.ListenAndServe(":8080", r.HandleRequest)
+	r := fin.Default("/api")
+	r.Use(fin.AllowMethodMiddleware("GET"))
+	{
+		r.ANY("/v1/hello", fin.HandlerFunc(func(ctx *fasthttp.RequestCtx) {
+			ctx.WriteString("hello world")
+		}))
+		r.ANY("/v2/hello", fin.HandlerFunc(func(ctx *fasthttp.RequestCtx) {
+			ctx.WriteString("你好")
+		}))
+		r.GET("/panic", fin.HandlerFunc(func(ctx *fasthttp.RequestCtx) {
+			panic("just for panic")
+		}))
+	}
+	fasthttp.ListenAndServe(":8080", r.HandleRequest)
 }
