6309712e0a011aca424f2875d23197538081bdfd
xsymphony <xiongshengyao@hotmail.com>
2020-01-04 15:07:21 +0800

    [v0.0.2]uri与路由函数的存储移动到engine中，router仅作代理

--- fin.go
+++ fin.go
@@ -1,22 +1,48 @@
 package fin
 
-import "github.com/valyala/fasthttp"
+import (
+	"fmt"
+
+	"github.com/valyala/fasthttp"
+)
 
 type Engine struct {
 	Router
+
+	handlers map[string]fasthttp.RequestHandler
 }
 
 func New() *Engine {
 	engine := &Engine{
 		Router: Router{
-			path:     "",
-			handlers: make(map[string]fasthttp.RequestHandler),
+			path: "",
 		},
+		handlers: make(map[string]fasthttp.RequestHandler),
 	}
 	engine.Router.engine = engine
 	return engine
 }
 
+func (e *Engine) addRouter(path string, h fasthttp.RequestHandler) {
+	// 从根router中查找路由是否存在
+	if _, ok := e.handlers[path]; ok {
+		panic(fmt.Sprintf("duplicate uri %s", path))
+	}
+	// 注册路由函数到根router中
+	e.handlers[path] = h
+}
+
+func (e *Engine) dispatch(ctx *fasthttp.RequestCtx) {
+	uri := string(ctx.Path())
+	h, ok := e.handlers[uri]
+	if !ok {
+		ctx.SetStatusCode(404)
+		ctx.WriteString("404 NOT FOUND")
+		return
+	}
+	h(ctx)
+}
+
 func (e *Engine) Run(addr string) error {
-	return fasthttp.ListenAndServe(addr, e.HandleRequest)
+	return fasthttp.ListenAndServe(addr, e.dispatch)
 }
--- router.go
+++ router.go
@@ -1,27 +1,20 @@
 package fin
 
 import (
-	"fmt"
-
 	"github.com/valyala/fasthttp"
 )
 
 type Router struct {
-	engine   *Engine
-	path     string                             // prefix是整个路由的公共前缀
-	handlers map[string]fasthttp.RequestHandler // 存放所有handle函数与url的映射关系
+	engine *Engine
+	path   string
 }
 
 // AddRouter 注册一个新的路由函数
 func (r *Router) AddRouter(relativePath string, h fasthttp.RequestHandler) {
 	// 计算路由的绝对路径
 	path := r.path + relativePath
-	// 从根router中查找路由是否存在
-	if _, ok := r.engine.handlers[path]; ok {
-		panic(fmt.Sprintf("duplicate uri %s", path))
-	}
-	// 注册路由函数到根router中
-	r.engine.handlers[path] = h
+	// 注册路由
+	r.engine.addRouter(path, h)
 }
 
 // Group新建一个路由分组
@@ -29,22 +22,9 @@ func (r *Router) Group(relativePath string) *Router {
 	// 计算路由的绝对路径
 	path := r.path + relativePath
 	router := &Router{
-		path:     path,
-		handlers: make(map[string]fasthttp.RequestHandler),
-		engine:   r.engine,
+		path:   path,
+		engine: r.engine,
 	}
 
 	return router
-}
-
-// HandleRequest 作为fasthttp总的入口函数，进行路由分发
-func (r *Router) HandleRequest(ctx *fasthttp.RequestCtx) {
-	uri := string(ctx.Path())
-	h, ok := r.handlers[uri]
-	if !ok {
-		ctx.SetStatusCode(404)
-		ctx.WriteString("404 NOT FOUND")
-		return
-	}
-	h(ctx)
 }
